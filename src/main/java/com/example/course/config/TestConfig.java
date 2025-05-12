fdddb7b5578265fea6dd2d5e2df2d5674547c57a
Eliane Turcilio <yuritb2008@gmail.com>
2025-05-12 18:41:31 -0300

    Enum OrderStatus

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.example.course.entities.Order;
 import com.example.course.entities.User;
+import com.example.course.entities.enums.OrderStatus;
 import com.example.course.repositories.OrderRepository;
 import com.example.course.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maria", "maria@gmail.com", "98435454", "password123");
 		User u2 = new User(null, "Bob", "bob@uol.com", "99120223", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2024-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2025-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2025-08-22T15:21:22Z"), u2);
+		Order o1 = new Order(null, Instant.parse("2024-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2025-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2025-08-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u2);
 		
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
